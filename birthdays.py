9fde4d9867ab002645e19f90f5b78c4ca4ec18be
sofifavaro <sofia.favaro@student.h-farm.com>
2021-11-24 14:41:50 +0100

    create a new branch called sofia

--- birthdays.py
+++ birthdays.py
@@ -19,6 +19,9 @@ birthdays = {
 
 
 def print_birthdays():
+
+'''Print what the function do'''
+
     print('Welcome to the birthday dictionary. We know the birthdays of these people:')
     for name in birthdays:
         print(name)
